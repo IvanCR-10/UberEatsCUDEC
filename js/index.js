40d75a8e9c3fc81bed2e6aec44eb69477b0e0cbe
IvanCR-10 <jcaudilloreyes@gmail.com>
2026-06-04 21:25:17 -0600

    Mostrar platillos en la aplicación

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo');
+let contenido = '';
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -11,4 +11,29 @@ document.addEventListener('DOMContentLoaded', function() {
 btnAgregarPlatillo.addEventListener('click', function(){
   alert('Platillo agregado')
 
+ 
+
 });
+
+ function mostrarPlatillo(platillo, id) {
+  contenido += ` 
+  <div class="card-panel recipe white row" data-id="${id}">
+  <div class="recipe-details">
+  <div class="recipe-title">
+  ${platillo.nombre}
+  </div>
+
+  <div class="recipe-ingredients">
+  ${platillo.ingredientes}
+ </div>
+  <div class="recipe-precio">
+  ${platillo.precio}
+  </div>
+  </div>
+  <div class="recipe-delete">
+  <i class="materiaL-icons" data-id="${id}">delete_outline</i>
+  </div>
+  
+  </div>`;
+  document.querySelector('.recipes').innerHTML = contenido;
+ } 

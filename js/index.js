34c732ea3d414cd40c5d6082b0c3cec4ac45d01c
IvanCR-10 <jcaudilloreyes@gmail.com>
2026-06-13 00:56:34 -0600

    clase17dejunio

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 let contenido = '';
+
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
   const menus = document.querySelectorAll('.side-menu');
@@ -9,31 +10,36 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 btnAgregarPlatillo.addEventListener('click', function(){
-  alert('Platillo agregado')
-
- 
+   alert('Platillo agregado');
 
 });
 
- function mostrarPlatillo(platillo, id) {
-  contenido += ` 
-  <div class="card-panel recipe white row" data-id="${id}">
-  <div class="recipe-details">
-  <div class="recipe-title">
-  ${platillo.nombre}
+function mostrarPlatillo(platillo, id){
+ contenido += `
+ <div class="card-panel recipe white row" id="${id}">
+ <div class="recipe-details">
+ <div class="recipe-title">
+      ${platillo.nombre}
   </div>
-
-  <div class="recipe-ingredients">
-  ${platillo.ingredientes}
+  <div class="recipe-ingredientes">
+      ${platillo.ingredientes}
  </div>
-  <div class="recipe-precio">
-  ${platillo.precio}
-  </div>
+        <div class="recipe-price">
+      ${platillo.precio}
+      
+    </div>
   </div>
   <div class="recipe-delete">
-  <i class="materiaL-icons" data-id="${id}">delete_outline</i>
+      <i class="material-icons" data-id="${id}">delete_outline</i>
   </div>
-  
   </div>`;
-  document.querySelector('.recipes').innerHTML = contenido;
- } 
+ document.querySelector('.recipes').innerHTML = contenido;
+
+}
+  
+function actualizarPlatillo(platillo, id) { 
+  let tarjeta = document. getElementById(`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+  tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
+}
